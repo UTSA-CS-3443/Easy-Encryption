e3244bdae1221f86cc02d28c7435823cc728fed7
NexeIV <NexeIV@DESKTOP-C802UP9.attlocal.net>
2022-11-10 22:03:57 -0600

    Created loaders.java class Updated Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 /**
@@ -11,16 +13,30 @@ import javafx.scene.layout.BorderPane;
  * @author Jacob Houssian
  * @author Abdurrahmaan Baghdadi
  * @author Preaston Cunningham
+ * @author Daniel Botello IV
  */
 public class Main extends Application {
+	//Makes stage available everywhere.
+	public static Stage stage;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage = primaryStage;
+			
+			//Connect to the FXML and load it in
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("/application/view/LoginView.fxml"));
+			AnchorPane layout = (AnchorPane) loader.load();
+			//Put the layout onto the scene
+			Scene scene = new Scene( layout );
+			//Load CSS
+			String css = this.getClass().getResource("application.css").toExternalForm();
+			scene.getStylesheets().add(css);
+			
+			//Set the scene on the stage
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
