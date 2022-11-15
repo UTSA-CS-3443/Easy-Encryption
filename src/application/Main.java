61746aab1174214ffd9e2e275b132b204f4cbdd7
Jacob Houssian <hoo761@users.noreply.github.com>
2022-11-15 17:04:32 -0600

    Merge (#1)
    * Add files via upload
    
    * Add files via upload
    
    New controllers
    
    * Update UserInteraction.fxml
    
    * Add files via upload
    
    new views
    
    * Move files out of nested folders
    
    * Added back Loaders class
    
    * Fixed stage name
    
    Co-authored-by: pedro <davila1pedro@gmail.com>
    Co-authored-by: Jacob Houssian <jacobrhoussian@gmail.com>

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,48 +1,32 @@
 package application;
 	
+import java.io.IOException;
+import application.controller.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
-/**
- * The Main class test
- * 
- * @author Jacob Houssian
- * @author Abdurrahmaan Baghdadi
- * @author Preaston Cunningham
- * @author Daniel Botello IV
- */
 public class Main extends Application {
-	//Makes stage available everywhere.
-	public static Stage stage;
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			stage = primaryStage;
-			
-			//Connect to the FXML and load it in
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(Main.class.getResource("/application/view/LoginView.fxml"));
-			AnchorPane layout = (AnchorPane) loader.load();
-			//Put the layout onto the scene
-			Scene scene = new Scene( layout );
-			//Load CSS
-			String css = this.getClass().getResource("application.css").toExternalForm();
-			scene.getStylesheets().add(css);
-			
-			//Set the scene on the stage
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+	public static Stage primaryStage;
+	public static BorderPane layout;
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		FXMLLoader loader = new FXMLLoader(Main.class.getResource("view/Login.fxml"));
+		try { Main.layout = (BorderPane) loader.load();	}
+		catch(IOException e) { e.printStackTrace(); } 
+		Main.layout.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
+		LoginController controller = loader.getController();
+		Main.layout.setCenter(controller.ap);
+		Scene scene = new Scene(Main.layout); 
+		Main.primaryStage = primaryStage;
+		Main.primaryStage.setScene(scene); 
+		Main.primaryStage.show(); 
 	}
 }
---
+++ src/application/controller/UserInteractionController.java
@@ -0,0 +1,33 @@
+package application.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+
+public class UserInteractionController implements Initializable {
+	@FXML
+	private AnchorPane outerAp, innerAp;
+	@FXML
+	private HBox buttons;
+	@FXML
+	private Button encrypt, decrypt, vault, savedKeys;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		outerAp.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
+		outerAp.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+		buttons.setSpacing(10);
+        buttons.setPadding(new Insets(15, 15, 15, 18));
+        buttons.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
+        innerAp.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
+	} 
+}
