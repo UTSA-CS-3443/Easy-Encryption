d94a049473665380d1460b483ea3f0baa73b0fa2
Abdurrahmaan-Baghdadi <40675016+Abdurrahmaan-Baghdadi@users.noreply.github.com>
2022-11-23 21:54:42 -0600

    Encryption Addition
    Encryption working

---
+++ src/application/model/CryptoUtilsTest.java
@@ -0,0 +1,25 @@
+package application.model;
+
+import java.io.File;
+ 
+/**
+ * A tester for the CryptoUtils class.
+ * @author www.codejava.net
+ *
+ */
+public class CryptoUtilsTest {
+    public static void main(String[] args) {
+        String key = "!@#kldhaskldfjks";
+        File inputFile = new File("data/document.txt");
+        File encryptedFile = new File("data/document.encrypted");
+        File decryptedFile = new File("data/document.decrypted");
+         
+        try {
+            CryptoUtils.encrypt(key, inputFile, encryptedFile);
+            CryptoUtils.decrypt(key, encryptedFile, decryptedFile);
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+}
