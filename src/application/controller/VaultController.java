35b320ab17962bce20e2e6d0a6c5a3fd4b12738a
pedro <davila1pedro@gmail.com>
2022-11-24 23:04:30 -0600

    Update VaultController.java

--- src/application/controller/VaultController.java
+++ src/application/controller/VaultController.java
@@ -33,8 +33,9 @@ public class VaultController implements EventHandler<ActionEvent>, Initializable
 		outerAp.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
 		outerAp.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
 		buttons.setSpacing(10);
-        buttons.setPadding(new Insets(15, 15, 15, 18));
-        buttons.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
+        buttons.setPadding(new Insets(30, 30, 30, 18));
+        //buttons.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
+        buttons.getStyleClass().add("hbox");
         innerAp.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
         state.setText("vault test");
 	}
