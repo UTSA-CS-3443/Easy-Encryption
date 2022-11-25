c492c5443dd782d654e4bff3e592e541b98489b3
Daniel Botello IV <NexeIV@DESKTOP-C802UP9>
2022-11-25 14:14:52 -0600

    New handleCircle method and setImages method

--- src/application/controller/UserInteractionController.java
+++ src/application/controller/UserInteractionController.java
@@ -9,12 +9,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 
 public class UserInteractionController implements Initializable {
     @FXML
@@ -23,9 +28,13 @@ public class UserInteractionController implements Initializable {
     private HBox buttons;
     @FXML
     private Button encrypt, decrypt, vault, savedKeys;
+    @FXML
+    private Circle eCircle, dCircle, vCircle, skCircle;
+    private Loaders loader;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    	loader = new Loaders();
         outerAp.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
         outerAp.setBackground(new Background(
                 new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
@@ -34,10 +43,14 @@ public class UserInteractionController implements Initializable {
         //buttons.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
         buttons.getStyleClass().add("hbox");
         innerAp.setStyle("-fx-border-color: black; -fx-border-width: 3px 3px 3px 3px");
+        //Sets background color to blue after CSS is applied. Delete bottom line for default value of white. -dbiv
+        innerAp.setStyle("-fx-background-color: #0198fc;");
+        
+        setImages();
     }
     
     public void handle(ActionEvent event) {
-    	Loaders loader = new Loaders();
+    	
         Button button = (Button) event.getSource();
         String buttonText = button.getText();
         
@@ -49,5 +62,36 @@ public class UserInteractionController implements Initializable {
             loader.loadSceneVault();
         else if (buttonText.equals("Saved Keys"))
             loader.loadSceneKeys();
+    }
+    
+    /**
+     * handleCircle handle mouse events generated from the 4 circles.
+     * 
+     * @param e
+     */
+    public void handleCircle(MouseEvent e) {
+    	Circle circle = (Circle) e.getSource();
+		String circleText = circle.getId();
+		
+		if (circleText.equals("eCircle")) loader.loadSceneEncrypt();
+		else if (circleText.equals("dCircle")) loader.loadSceneDecrypt();
+		else if (circleText.equals("vCircle")) loader.loadSceneVault();
+		else if (circleText.equals("skCircle")) loader.loadSceneKeys();
+    }
+    
+    /**
+     * Cleans up code in initialize method. Sets images within each circle.
+     * 
+     * 
+     */
+    public void setImages() {
+    	 eCircle.setFill(new ImagePattern(new Image("file:data/encrypt.png", false)));
+         eCircle.setEffect(new DropShadow(+25d, 0d, +2d, Color.RED));
+         dCircle.setFill(new ImagePattern(new Image("file:data/decrypt.png", false)));
+         dCircle.setEffect(new DropShadow(+25d, 0d, +2d, Color.BLUE));
+         vCircle.setEffect(new DropShadow(+25d, 0d, +2d, Color.WHITE));
+         vCircle.setFill(new ImagePattern(new Image("file:data/vault.jpg", false)));
+         skCircle.setEffect(new DropShadow(+25d, 0d, +2d, Color.WHITE));
+         skCircle.setFill(new ImagePattern(new Image("file:data/keys.png", false)));
     }
 }
